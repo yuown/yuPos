c92f91b8bfda8fef57de07ee647b6b686064df61
yenkaykiran <yenkaykiran@gmail.com>
2016-12-23 08:43:34 +0530

    Updated Schema

--- schema/test.sql
+++ schema/test.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `test`;
 -- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
 --
 -- Host: 127.0.0.1    Database: test
 -- ------------------------------------------------------
--- Server version	5.6.25
+-- Server version	5.7.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,53 +16,85 @@ USE `test`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `elements`
+-- Table structure for table `element`
 --
 
-DROP TABLE IF EXISTS `elements`;
+DROP TABLE IF EXISTS `element`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `elements` (
+CREATE TABLE `element` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT '1',
-  `rank` int(11) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `elements`
+-- Dumping data for table `element`
 --
 
-LOCK TABLES `elements` WRITE;
-/*!40000 ALTER TABLE `elements` DISABLE KEYS */;
-INSERT INTO `elements` VALUES (1,'Eatables',1,1),(2,'Cosmetics',1,2),(3,'Kitchen Related',1,3),(4,'Vegetables',1,1),(5,'Fruits',1,2),(6,'Dry Fruits',1,3),(7,'Soaps',1,1),(8,'Edible Oil',1,1),(9,'Vessels',1,1);
-/*!40000 ALTER TABLE `elements` ENABLE KEYS */;
+LOCK TABLES `element` WRITE;
+/*!40000 ALTER TABLE `element` DISABLE KEYS */;
+INSERT INTO `element` VALUES (1,'Receiving','Receiving',1),(2,'Quarantine','Quarantine',1),(3,'Reserve','Reserve',1),(4,'Picking','Picking',1),(5,'Work In Process','Work In Process',1),(6,'Finished Goods','Finished Goods',1);
+/*!40000 ALTER TABLE `element` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `levels`
+-- Table structure for table `level`
 --
 
-DROP TABLE IF EXISTS `levels`;
+DROP TABLE IF EXISTS `level`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `levels` (
+CREATE TABLE `level` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `levels`
+-- Dumping data for table `level`
 --
 
-LOCK TABLES `levels` WRITE;
-/*!40000 ALTER TABLE `levels` DISABLE KEYS */;
-INSERT INTO `levels` VALUES (1,'Category'),(2,'Sub-Category');
-/*!40000 ALTER TABLE `levels` ENABLE KEYS */;
+LOCK TABLES `level` WRITE;
+/*!40000 ALTER TABLE `level` DISABLE KEYS */;
+INSERT INTO `level` VALUES (1,'Location Type','Location Type',1);
+/*!40000 ALTER TABLE `level` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `level_element`
+--
+
+DROP TABLE IF EXISTS `level_element`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `level_element` (
+  `level_id` int(11) NOT NULL,
+  `element_id` int(11) NOT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`level_id`,`element_id`),
+  KEY `level_id_fk_idx` (`level_id`),
+  KEY `element_id_fk_idx` (`element_id`),
+  CONSTRAINT `element_id_fk_1` FOREIGN KEY (`element_id`) REFERENCES `element` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `level_id_fk_1` FOREIGN KEY (`level_id`) REFERENCES `level` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `level_element`
+--
+
+LOCK TABLES `level_element` WRITE;
+/*!40000 ALTER TABLE `level_element` DISABLE KEYS */;
+INSERT INTO `level_element` VALUES (1,1,1,1),(1,2,1,1),(1,3,1,1),(1,4,1,1),(1,5,1,1),(1,6,1,1);
+/*!40000 ALTER TABLE `level_element` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,6 +108,7 @@ CREATE TABLE `list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -88,35 +119,36 @@ CREATE TABLE `list` (
 
 LOCK TABLES `list` WRITE;
 /*!40000 ALTER TABLE `list` DISABLE KEYS */;
-INSERT INTO `list` VALUES (1,'Categories','Categories Description');
+INSERT INTO `list` VALUES (1,'Location Types','Inventory Location Types',1);
 /*!40000 ALTER TABLE `list` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `list_elements`
+-- Table structure for table `list_level`
 --
 
-DROP TABLE IF EXISTS `list_elements`;
+DROP TABLE IF EXISTS `list_level`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `list_elements` (
-  `element` int(11) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
-  KEY `element_id_idx` (`element`),
-  KEY `level_id_idx` (`level`),
-  CONSTRAINT `element_id` FOREIGN KEY (`element`) REFERENCES `elements` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `level_id` FOREIGN KEY (`level`) REFERENCES `levels` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `list_level` (
+  `list_id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `rank` int(11) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`list_id`,`level_id`),
+  KEY `list_id_fk_idx` (`list_id`),
+  KEY `level_id_fk_idx` (`level_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `list_elements`
+-- Dumping data for table `list_level`
 --
 
-LOCK TABLES `list_elements` WRITE;
-/*!40000 ALTER TABLE `list_elements` DISABLE KEYS */;
-INSERT INTO `list_elements` VALUES (1,1);
-/*!40000 ALTER TABLE `list_elements` ENABLE KEYS */;
+LOCK TABLES `list_level` WRITE;
+/*!40000 ALTER TABLE `list_level` DISABLE KEYS */;
+INSERT INTO `list_level` VALUES (1,1,1,1);
+/*!40000 ALTER TABLE `list_level` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -128,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-12-21  8:58:27
+-- Dump completed on 2016-12-23  8:40:54
